c007b206ee5115272a4587629425ca7968b6d03c
ukigumo-shiina3 <daisukeyao@outlook.jp>
2024-07-02 20:58:32 +0900

    🗃️ スキーマ定義の拡張

--- src/server.js
+++ src/server.js
@@ -1,14 +1,30 @@
 const { ApolloServer, gql } = require("apollo-server");
 
+let links = [
+  {
+    id: "link-0",
+    description: "GraphQLを学ぶ",
+    url: "www.graphql.com",
+  },
+];
+
 const typeDefs = gql`
   type Query {
     info: String!
+    feed: [Link]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `;
 
 const resolvers = {
   Query: {
     info: () => "HackerNews",
+    feed: () => links,
   },
 };
 
