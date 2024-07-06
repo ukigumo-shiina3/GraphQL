6c1c033948968b4f6cea21009715289f1e046e1c
ukigumo-shiina3 <daisukeyao@outlook.jp>
2024-07-06 18:19:19 +0900

    ✨ Prismaクライアントでデータベース操作

---
+++ src/script.js
@@ -0,0 +1,22 @@
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+async function main() {
+    const newLink = await prisma.link,create({
+        data: {
+            description: "GraphQLを学ぶ",
+            url: "www.graphql.com",
+        },
+    });
+    const allLinks = await prisma.link.findMany();
+    console.log(allLinks);
+}
+
+main()
+    .catch((e) => {
+        throw e;
+    })
+    .finally(async () => {
+        prisma.$disconnect;
+    })
