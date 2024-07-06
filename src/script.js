f4db5484661737beccfa4c495c1278a618cf9ef0
ukigumo-shiina3 <daisukeyao@outlook.jp>
2024-07-06 19:43:20 +0900

    ♻️ タイポの修正

--- src/script.js
+++ src/script.js
@@ -3,20 +3,20 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function main() {
-    const newLink = await prisma.link,create({
-        data: {
-            description: "GraphQLを学ぶ",
-            url: "www.graphql.com",
-        },
-    });
-    const allLinks = await prisma.link.findMany();
-    console.log(allLinks);
+  const newLink = await prisma.link.create({
+    data: {
+      description: "GraphQLを学ぶ",
+      url: "www.graphql.com",
+    },
+  });
+  const allLinks = await prisma.link.findMany();
+  console.log(allLinks);
 }
 
 main()
-    .catch((e) => {
-        throw e;
-    })
-    .finally(async () => {
-        prisma.$disconnect;
-    })
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    prisma.$disconnect;
+  });
