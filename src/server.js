da6edd252cfb0991b9136239723792a886256c06
ukigumo-shiina3 <daisukeyao@outlook.jp>
2024-07-07 22:39:16 +0900

    ✨ サーバーとPrismaを連携

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ const { ApolloServer, gql } = require("apollo-server");
 const fs = require("fs");
 const path = require("path");
 
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
 let links = [
   {
     id: "link-0",
@@ -35,6 +39,9 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf-8"),
   resolvers,
+  context: {
+    prisma,
+  },
 });
 
 server.listen().then(({ url }) => console.log(`${url}でサーバーを起動中`));
