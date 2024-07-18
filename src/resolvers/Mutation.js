229ec12171991372f2be370d0df9d166e5fc61e6
ukigumo-shiina3 <daisukeyao@outlook.jp>
2024-07-18 23:01:08 +0900

    ✨ニュース投稿用のリゾルバを作成

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -44,3 +44,13 @@ async function login(parent, args, context) {
     user,
   };
 }
+
+// ニュースを投稿するリゾルバ
+async function post(parent, args, context) {
+  return await context.prisma.link.create({
+    data: {
+      url: args.url,
+      description: args.description,
+    },
+  });
+}
